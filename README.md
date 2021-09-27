4b3eaba08f8726a067514969fc55d11beccbd9fe
Andrew N. Shalaev <isqad88@gmail.com>
2021-09-27 11:45:04 +0500

    add interceprors

--- README.md
+++ README.md
@@ -1,13 +1,68 @@
-<h1 align="center">Firebase auth service</h1>
+<h1 align="center">Firebase JWT grpc-auth service</h1>
 
-Service for verification of the Firebase JWT
+Grpc-service for verification of the Firebase JWTs
+
+# Usage
+
+1) Integrate auth interceptors into your grpc-project:
+
+```go
+package main
+
+import (
+  "net"
+  "google.golang.org/grpc"
+  auth "github.com/isqad/firebase-auth-service/pkg/service"
+)
+
+type UsersAPIServer struct {
+	UnimplementedUsersServer
+}
+
+func NewUsersAPIServer() (*UsersAPIServer, error) {
+  return &UsersAPIServer{}
+}
+
+func (s *UsersAPIServer) Start() error {
+  authAddr := os.Getenv("FIREBASE_AUTH_SERVICE_ADDRESS")
+
+  l, err := net.Listen("tcp", authAddr)
+	if err != nil {
+		return err
+	}
+
+  a := auth.NewAuthInterceptor(authAddr)
+
+	g := grpc.NewServer(
+		grpc.UnaryInterceptor(a.AuthUnaryInterceptor),
+    grpc.StreamInterceptor(a.AuthStreamInterceptor),
+	)
+	RegisterUsersServer(g, s)
+	if err := g.Serve(l); err != nil {
+		return err
+	}
+
+	return nil
+}
+```
+
+2) Build and run the service:
+
+```bash
+docker build -t your-name:firebase-auth-service:0.1.0 .
+```
+
+```bash
+docker run --rm -d -p 50053:50053 --name firebase-auth-service your-name:firebase-auth-service your-name:firebase-auth-service:0.1.0
+```
+
+3) Run your grpc-service.
 
 # Roadmap
 
 - [x] Grabing google pub keys
 - [x] Verification firebase auth JWT of User and extract his ID
-- [ ] grpc auth interceptors for services
+- [x] grpc auth interceptors for services
 - [x] Configuration
-- [ ] Deployment
 - [ ] Tests
 - [x] grpc error codes
---
+++ pkg/service/interceptors.go
@@ -0,0 +1,86 @@
+package service
+
+import (
+	"context"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+// AuthInterceptor is interface for declare auth interceptor
+type AuthInterceptor interface {
+	GetFirebaseAuthServiceAddr() string
+}
+
+type interceptor struct {
+	addr string
+}
+
+// NewAuthInterceptor returns new instance of auth interceptor
+func NewAuthInterceptor(authServiceAddress string) AuthInterceptor {
+	return &interceptor{
+		addr: authServiceAddress,
+	}
+}
+
+func (i *interceptor) GetFirebaseAuthServiceAddr() string {
+	return i.addr
+}
+
+// AuthUnaryInterceptor send jwt to auth service before handle unary request
+func (i *interceptor) AuthUnaryInterceptor(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (interface{}, error) {
+	err := i.authenticate(ctx, info.FullMethod)
+	if err != nil {
+		return nil, err
+	}
+
+	return handler(ctx, req)
+}
+
+// AuthStreamInterceptor returns stream interceptor for authenticate
+func (i *interceptor) AuthStreamInterceptor(
+	srv interface{},
+	stream grpc.ServerStream,
+	info *grpc.StreamServerInfo,
+	handler grpc.StreamHandler,
+) error {
+	err := i.authenticate(stream.Context(), info.FullMethod)
+	if err != nil {
+		return err
+	}
+
+	return handler(srv, stream)
+}
+
+func (i *interceptor) authenticate(ctx context.Context, method string) error {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok || len(md["authorization"]) == 0 {
+		return status.Errorf(codes.Unauthenticated, "Wrong metadata: %v", md)
+	}
+
+	conn, err := grpc.Dial(i.GetFirebaseAuthServiceAddr(), []grpc.DialOption{
+		grpc.WithInsecure(),
+		grpc.WithBlock(),
+	}...)
+	if err != nil {
+		return status.Errorf(codes.Internal, "Could not to dial auth server")
+	}
+	defer conn.Close()
+	authClient := NewAuthClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_, err = authClient.Verify(ctx, &Token{Token: md["authorization"][0]})
+	if err != nil {
+		return status.Errorf(codes.Unauthenticated, "Could not to verify auth token")
+	}
+
+	return nil
+}
